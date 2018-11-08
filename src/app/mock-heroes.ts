6ba86341a81b3922619a0c327cc88451f9665c68
Chris <christian-boer@hotmail.com>
2018-11-08 19:02:32 +0100

    Master-Detail components

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Pianus'},
+    { id: 12, name: 'Zakum'},
+    { id: 13, name: 'Pink Bean'},
+    { id: 14, name: 'Mushmom'},
+    { id: 15, name: 'Crimson Balrog'},
+    { id: 16, name: 'Alishar'},
+    { id: 17, name: 'Horntail'},
+    { id: 18, name: 'Magnus'},
+    { id: 19, name: 'Cygnus'},
+    { id: 20, name: 'Papulatus'}
+]
